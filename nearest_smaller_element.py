35b5e6a8b081dc4e0ff96e64fc90090bb43ecff0
M Manzur Morshed <mmorshed@asu.edu>
2018-03-02 20:41:55 -0500

    Create nearest_smaller_element.py

---
+++ nearest_smaller_element.py
@@ -0,0 +1,47 @@
+#Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
+#
+#More formally,
+#
+#G[i] for an element A[i] = an element A[j] such that 
+#    j is maximum possible AND 
+#    j < i AND
+#    A[j] < A[i]
+#Elements for which no smaller element exist, consider next smaller element as -1.
+#
+#Example:
+#
+#Input : A : [4, 5, 2, 10, 8]
+#Return : [-1, 4, -1, 2, 2]
+#
+#Example 2:
+#
+#Input : A : [3, 2, 1]
+#Return : [-1, -1, -1]
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def prevSmaller(self, A):
+        stack = []
+        return_arr = []
+        if len(A) == 0:
+            return []
+        i = 1
+        stack.append(A[0])
+        return_arr.append(-1)
+        if len(A) > 1:
+            j = len(stack) - 1
+            while(i<len(A)):
+                if j < 0:
+                    return_arr.append(-1)
+                    stack.append(A[i])
+                    j = len(stack) - 1
+                    i = i + 1
+                elif stack[j] > A[i] or stack[j] == A[i]:
+                    del stack[j]
+                    j = len(stack) - 1
+                elif stack[j] < A[i]:
+                    return_arr.append(stack[j])
+                    stack.append(A[i])
+                    i = i + 1
+                    j = len(stack) - 1
+        return return_arr
